475e9e552719044ff3b7ef203290d7684e44769f
u4bi <admin@naver.id>
2018-04-27 09:59:28 +0900

    Added ring/ring-json to project.clj

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.6.3"]
                  [ring/ring-jetty-adapter "1.6.3"]
-                 [compojure "1.6.1"]]
+                 [compojure "1.6.1"]
+                 [ring/ring-json "0.4.0"]]
   :plugins [[lein-ring "0.12.4"]]
   :ring {:handler short-url.core/handler})
