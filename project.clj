9acc0d91dd69e37bf55eb347c6b971d5830fde50
u4bi <admin@naver.id>
2018-04-27 09:39:38 +0900

    Added compojure to project.clj

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.6.3"]
-                 [ring/ring-jetty-adapter "1.6.3"]]
+                 [ring/ring-jetty-adapter "1.6.3"]
+                 [compojure "1.6.1"]]
   :plugins [[lein-ring "0.12.4"]]
   :ring {:handler short-url.core/handler})
