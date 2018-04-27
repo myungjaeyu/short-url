d5ec4b1ec2f854884c24058b65c8f6b2f003e63e
u4bi <admin@naver.id>
2018-04-27 09:42:42 +0900

    Added compojure handler.

--- src/short_url/core.clj
+++ src/short_url/core.clj
@@ -1,6 +1,7 @@
-(ns short-url.core)
+(ns short-url.core
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]))
 
-(defn handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "<h1>u4bi</h1>"})
+(defroutes handler
+  (GET "/" [] "<h1>u4bi</h1>")
+  (route/not-found "<h1>404</h1>"))
