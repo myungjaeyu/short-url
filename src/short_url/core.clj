6fab877541c3e7440c03fd463444fb480e380b81
u4bi <admin@naver.id>
2018-04-27 18:34:51 +0900

    Added functions for short-url.

--- src/short_url/core.clj
+++ src/short_url/core.clj
@@ -1,12 +1,35 @@
 (ns short-url.core
   (:use ring.util.response)
   (:require [compojure.core :refer :all]
+            [compojure.handler :as h]
             [compojure.route :as route]
-            [ring.middleware.json :refer [wrap-json-response]]))
+            [ring.middleware.json :refer [wrap-json-response]]
+            [short-url.db :as db]
+            [short-url.util :refer [gen-key]]))
+
+(defn insert-url [url]
+  (db/insert-url (gen-key) url))
+
+(defn get-url [key]
+  (db/get-url key))
+
+(defn delete-url [key]
+  (let [deleted (db/delete-url key)]
+    {:deleted (cond 
+                (= deleted 1) true
+                :else false)}))
 
 (defroutes handler
-  (GET "/" [] (response {:message "u4bi"}))
-  (route/not-found (response {:message "not found"})))
+  (context "/api" []
+           (POST "/" [url] (response
+                            (insert-url url)))
+           (GET "/:key" [key] (response
+                               (get-url key)))
+           (DELETE "/" [key] (response
+                              (delete-url key))))
+  (route/not-found (response
+                    {:message "not found"})))
 
 (def app
-  (wrap-json-response handler))
+  (wrap-json-response 
+   (h/api handler)))
